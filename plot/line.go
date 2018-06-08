f3fee1e5e4940beb06f8f682f6d9a5176d610caf
ksang <kaitoy@qq.com>
2018-06-09 02:15:45 +0800

    add vendor packages for plot

---
+++ plot/line.go
@@ -0,0 +1,110 @@
+package plot
+
+import (
+	"image/color"
+	"math/rand"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
+)
+
+func lineexample() {
+	rand.Seed(int64(0))
+
+	p, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+
+	p.Title.Text = "Plotutil example"
+	p.X.Label.Text = "X"
+	p.Y.Label.Text = "Y"
+
+	err = plotutil.AddLinePoints(p,
+		"First", randomPoints(15),
+		"Second", randomPoints(15),
+		"Third", randomPoints(15))
+	if err != nil {
+		panic(err)
+	}
+
+	// Save the plot to a PNG file.
+	if err := p.Save(4*vg.Inch, 4*vg.Inch, "points.png"); err != nil {
+		panic(err)
+	}
+}
+
+// randomPoints returns some random x, y points.
+func randomPoints(n int) plotter.XYs {
+	pts := make(plotter.XYs, n)
+	for i := range pts {
+		if i == 0 {
+			pts[i].X = rand.Float64()
+		} else {
+			pts[i].X = pts[i-1].X + rand.Float64()
+		}
+		pts[i].Y = pts[i].X + 10*rand.Float64()
+	}
+	return pts
+}
+
+func detailedExample() {
+	// Get some random points
+	rand.Seed(int64(0))
+	n := 15
+	scatterData := randomPoints(n)
+	lineData := randomPoints(n)
+	linePointsData := randomPoints(n)
+
+	// Create a new plot, set its title and
+	// axis labels.
+	p, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+	p.Title.Text = "Points Example"
+	p.X.Label.Text = "X"
+	p.Y.Label.Text = "Y"
+	// Draw a grid behind the data
+	p.Add(plotter.NewGrid())
+
+	// Make a scatter plotter and set its style.
+	s, err := plotter.NewScatter(scatterData)
+	if err != nil {
+		panic(err)
+	}
+	s.GlyphStyle.Color = color.RGBA{R: 255, B: 128, A: 255}
+
+	// Make a line plotter and set its style.
+	l, err := plotter.NewLine(lineData)
+	if err != nil {
+		panic(err)
+	}
+	l.LineStyle.Width = vg.Points(1)
+	l.LineStyle.Dashes = []vg.Length{vg.Points(5), vg.Points(5)}
+	l.LineStyle.Color = color.RGBA{B: 255, A: 255}
+
+	// Make a line plotter with points and set its style.
+	lpLine, lpPoints, err := plotter.NewLinePoints(linePointsData)
+	if err != nil {
+		panic(err)
+	}
+	lpLine.Color = color.RGBA{G: 255, A: 255}
+	lpPoints.Shape = draw.PyramidGlyph{}
+	lpPoints.Color = color.RGBA{R: 255, A: 255}
+
+	// Add the plotters to the plot, with a legend
+	// entry for each
+	p.Add(s, l, lpLine, lpPoints)
+	p.Legend.Add("scatter", s)
+	p.Legend.Add("line", l)
+	p.Legend.Add("line points", lpLine, lpPoints)
+
+	// Save the plot to a PNG file.
+	if err := p.Save(4*vg.Inch, 4*vg.Inch, "points.png"); err != nil {
+		panic(err)
+	}
+}
